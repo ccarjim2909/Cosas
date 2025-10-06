06f518f48a8ef99c99c801f9c9790cf38cf7d72e
ccarjim2909 <158286303+ccarjim2909@users.noreply.github.com>
2025-10-06 11:42:30 +0200

    terminado

---
+++ suma_binaria.py
@@ -0,0 +1,70 @@
+
+
+def lee_los_binarios():
+    binario1 = input("Ingresa el primer numero binario que quieras sumar: ")
+
+    while not set(binario1) <= {"1", "0"} or not len(binario1) == 8:
+        binario1 = input("Lo que has escrito no es un numero binario o no tiene 8 bits, vuelve a intentarlo: ")
+
+    binario2 = input("Ingresa el segundo numero binario que quieras sumar: ")
+
+    while not set(binario2) <= {"1", "0"} or not len(binario2) == 8:
+        binario2 = input("Lo que has escrito no es un numero binario o no tiene 8 bits, vuelve a intentarlo: ")
+
+
+    binario1 = list(binario1)
+
+    binario2 = list(binario2)
+
+    return binario1, binario2
+
+def suma_binarios(binario1, binario2):
+    acarreo = list("000000000")
+
+    resultado_invertido = []
+
+    for i in range(len(binario1) - 1, -1, -1):
+        if binario1[i] == '0' and binario2[i] == '0':
+            if acarreo[i+1] == '0':
+                resultado_invertido.append('0')
+            else:
+                resultado_invertido.append('1')
+        elif binario1[i] == '0' and binario2[i] == '1' or binario1[i] == '1' and binario2[i] == '0':
+            if acarreo[i+1] == '0':
+                resultado_invertido.append('1')
+            else:
+                resultado_invertido.append('0')
+                acarreo[i] = "1"
+        else:
+            if acarreo[i+1] == '0':
+                resultado_invertido.append('0')
+                acarreo[i] = "1"
+            else:
+                resultado_invertido.append("1")
+    if acarreo[0] == '1':
+        resultado_invertido.append('1')
+
+    resultado = resultado_invertido[::-1]
+    return resultado
+
+
+
+
+def main():
+
+    # Lee los operandos
+    binario1, binario2 = lee_los_binarios()
+
+    print("El primer numero binario es: ", binario1)
+    print("El segundo numero binario es: ", binario2)
+
+    # Realiza las operaciones
+    resultado = suma_binarios(binario1, binario2)
+
+    # Devuelve un resultado
+    print("El resultado la suma de estos dos numeros es: ", resultado)
+
+
+
+if __name__ == '__main__':
+    main()
