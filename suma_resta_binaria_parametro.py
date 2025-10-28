a0537e3da28376d26a0650a8d2f0cf3d2db70c0b
ccarjim2909 <158286303+ccarjim2909@users.noreply.github.com>
2025-10-28 21:01:13 +0100

    Falta por terminar el que sea menor el que resta a uno mayor

--- suma_resta_binaria_parametro.py
+++ suma_resta_binaria_parametro.py
@@ -2,22 +2,49 @@ import sys
 from sys import argv
 
 def lee_los_binarios():
-    binario1 = sys.argv[1]
 
-    if not set(binario1) <= {"1", "0"} or not len(binario1) == 8:
-        print("El primer numero que has escrito no es un numero binario o no tiene 8 bits, vuelve a intentarlo.")
-    else:
-        binario2 = sys.argv[2]
 
-        if not set(binario2) <= {"1", "0"} or not len(binario2) == 8:
-            print("El segundo numero que has escrito no es un numero binario o no tiene 8 bits, vuelve a intentarlo.")
+    if len(argv) == 3:
+
+        binario1 = sys.argv[1]
+
+        if not set(binario1) <= {"1", "0"} or not len(binario1) == 8:
+            print("El primer numero que has escrito no es un numero binario o no tiene 8 bits, vuelve a intentarlo.")
         else:
-            binario1 = list(binario1)
+            binario2 = sys.argv[2]
 
-            binario2 = list(binario2)
+            if not set(binario2) <= {"1", "0"} or not len(binario2) == 8:
+                print("El segundo numero que has escrito no es un numero binario o no tiene 8 bits, vuelve a intentarlo.")
+            else:
+                binario1 = list(binario1)
 
-            return binario1, binario2
+                binario2 = list(binario2)
 
+                return binario1, binario2
+
+    elif len(argv) == 4:
+        binario1 = sys.argv[1]
+        operando = sys.argv[2]
+
+        if not set(binario1) <= {"1", "0"} or not len(binario1) == 8:
+            print("El primer numero que has escrito no es un numero binario o no tiene 8 bits, vuelve a intentarlo.")
+        else:
+            binario2 = sys.argv[3]
+
+            if not set(binario2) <= {"1", "0"} or not len(binario2) == 8:
+                print(
+                    "El segundo numero que has escrito no es un numero binario o no tiene 8 bits, vuelve a intentarlo.")
+            else:
+                binario1 = list(binario1)
+
+                binario2 = list(binario2)
+
+                return binario1, operando, binario2
+
+    else:
+        print("El programa no se puede ejecutar como lo has escrito. /n"
+              "Intenta escribirlo de esta manera: python suma_resta_binaria_parametro.py binario1 operando binario2 /n"
+              "O si quieres hacer las dos operaciones no uses el operando: python suma_resta_binaria_parametro.py binario1 binario2")
 
 
 def suma_binarios(binario1, binario2):
@@ -56,72 +83,62 @@ def resta_binarios(binario1, binario2):
 
     resultado_resta_invertido = []
 
-000000000
- 00000000
- 00000000
 
-cuenta
-
- 00000000
- 00000000
-
-000000000
-resultado
-000000000
-
-
-#mirar
     for i in range(len(binario1) - 1, -1, -1):
         if binario1[i] == '0' and binario2[i] == '0':
-            if quitar[i+1] == '0':
+            if quitar[i + 1] == '0':
                 resultado_resta_invertido.append('0')
             else:
                 resultado_resta_invertido.append('1')
-        elif binario1[i] == '0' and binario2[i] == '1':
-            if quitar[i+1] == '0':
-                resultado_resta_invertido.append('1')
-            else:
-                resultado_resta_invertido.append('0')
-
-
-        elif binario1[i] == '1' and binario2[i] == '0':
-            if quitar[i+1] == '0':
-                resultado_resta_invertido.append('')
-            else:
-                quitar[i+1] = '0'
                 quitar[i] = '1'
-                resultado_resta_invertido.append('')
-
-
+        elif binario1[i] == '1' and binario2[i] == '0':
+            if quitar[i + 1] == '0':
+                resultado_resta_invertido.append('1')
+            else:
+                resultado_resta_invertido.append('0')
+        elif binario1[i] == '0' and binario2[i] == '1':
+            if quitar[i + 1] == '0':
+                resultado_resta_invertido.append('1')
+                quitar[i] = '1'
+            else:
+                resultado_resta_invertido.append('0')
+                quitar[i] = '1'
         else:
             if quitar[i + 1] == '0':
-                resultado_resta_invertido.append('')
+                resultado_resta_invertido.append('0')
             else:
-                quitar[i + 1] = '0'
-                quitar[i] = '1'
-                resultado_resta_invertido.append('')
+                resultado_resta_invertido.append('1')
 
-
+    resultado_resta = resultado_resta_invertido[::-1]
+    return resultado_resta
 
 
 
 def main():
+    datos = lee_los_binarios()
 
-    # Lee los operandos
-    binario1, binario2 = lee_los_binarios()
+    if datos and len(datos) == 2:
+        binario1, binario2 = datos
 
-    print("El primer numero binario es: ", binario1)
-    print("El segundo numero binario es: ", binario2)
+        resultado_suma = suma_binarios(binario1, binario2)
+        print("El resultado la suma de estos dos numeros es: ", resultado_suma)
 
-    # Realiza las operaciones
-    resultado_suma = suma_binarios(binario1, binario2)
-    resultado_resta = resta_binarios(binario1, binario2)
+        resultado_resta = resta_binarios(binario1, binario2)
+        print("El resultado la resta de estos dos numeros es: ", resultado_resta)
 
-    # Devuelve un resultado
-    print("El resultado la suma de estos dos numeros es: ", resultado_suma)
-    print("El resultado la suma de estos dos numeros es: ", resultado_resta)
+    elif datos and len(datos) == 3:
+        binario1, operando, binario2 = datos
 
+        if operando == '+':
+            resultado = suma_binarios(binario1, binario2)
+            print("El resultado la suma de estos dos numeros es: ", resultado)
 
+        elif operando == '-':
+            resultado = resta_binarios(binario1, binario2)
+            print("El resultado la resta de estos dos numeros es: ", resultado)
+
+        else:
+            print("Operando no valido. Usa '+' o '-'.")
 
 if __name__ == '__main__':
     main()
