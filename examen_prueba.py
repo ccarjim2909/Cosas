946a152478b153acab790d08125a48b9020cf9a6
ccarjim2909 <158286303+ccarjim2909@users.noreply.github.com>
2025-12-04 12:31:28 +0100

    Enhance MOVE and DISPLAY command functionality
    Added validation for variable names and limits in MOVE command. Implemented DISPLAY command functionality to show variable values.

--- examen_prueba.py
+++ examen_prueba.py
@@ -9,7 +9,7 @@ comando = ejecucion.split()
 
 reserva = ["MOVE","ADD","DISPLAY","STOP", "move", "add", "display", "stop"]
 memoria = {}
-
+display = []
 
 while len(comando) > 0 and comando[0] != "STOP.":
     if comando[0] == "MOVE":
@@ -20,12 +20,19 @@ while len(comando) > 0 and comando[0] != "STOP.":
                 if comando[3] in reserva:
                     print(comando[3] ,"es una palabra reservada del sistema y estas solo se pueden usar para ejecutar los comandos.\n")
 
-                elif not comando[3].isidentifier():
-                    print(comando[3] ,"contiene caracteres inválidos o empieza por número y no puede guardarse como nombre de una variable.\n")
+                elif len(comando[3]) > 5:
+                    print(comando[3] ,"solo puede tener una longitud maxima de 5 caracteres.\n")
+
+                elif not comando[3].isalpha() or comando[3].isupper():
+                    print(comando[3] ,"contiene caracteres inválidos, solo puede tener letras minusculas (a-z) y no puede guardarse como nombre de una variable.\n")
 
                 else:
+                    if len(comando[1]) > 5:
+                        comando[1] = int(comando[1]) % 100000
+
                     memoria[comando[3]] =  int(comando[1])
                     print (memoria,"\n")
+
             else:
                 print ("El comando no ha terminado con un punto\n")
         else:
@@ -36,22 +43,27 @@ while len(comando) > 0 and comando[0] != "STOP.":
             else:
                 print ("El comando MOVE necesita que le escribas TO para declararle a quien le quieres asignar el valor de tu variable\n")
 
-    elif comando[0] == "ADD":
-
-
-
     elif comando[0] == "DISPLAY":
+        if len(comando) > 1:
+            if comando[len(comando)-1].endswith("."):
+                comando[len(comando)-1] = comando[len(comando)-1][:-1]
+            for variable in comando[1:]:
+                if variable in memoria:
+                    display.append(str(memoria[variable]))
+            print (",".join(display), "\n")
+            display = []
+
+    elif comando[0] == "ADD":
+        print("d")
 
 
 
 
-
-
-    ejecucion = input("dime que quieres hacer.:\n"
-                      "MOVE (valor) TO (varialble).:\n"
-                      "ADD (valor) TO (varialble).:\n"
-                      "DISPLAY (variable).:\n"
-                      "STOP.:\n"
+    ejecucion = input("dime que quieres hacer.\n"
+                      "MOVE (valor) TO (varialble).\n"
+                      "ADD (valor) TO (varialble).\n"
+                      "DISPLAY (variable).\n"
+                      "STOP.\n"
                       ">>> ")
 
     comando = ejecucion.split()
