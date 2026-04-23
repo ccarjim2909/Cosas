0df5747014f355f50ee9bfbb80ebf987757837ec
ccarjim2909 <158286303+ccarjim2909@users.noreply.github.com>
2026-04-23 20:00:58 +0200

    1 Añadida clase Student

---
+++ zelda/domain/model/Student.kt
@@ -0,0 +1,10 @@
+package org.iesra.procesaalumnos.domain.model
+
+
+data class Student(
+    val nombre: String,
+    val apellido: String,
+    val emailGenerado: String,
+    val emailOriginal: String,
+    val grupoAsignado: String
+)
